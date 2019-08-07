7a8e39946c2d1adff3e69d8f43f259fe792c1793
Arvin Agas <arvagas@gmail.com>
2019-08-07 15:46:45 -0400

    implemented role select with validation

--- onboarding/src/components/RegistrationForm.js
+++ onboarding/src/components/RegistrationForm.js
@@ -12,18 +12,32 @@ function RegistrationForm({ values, errors, touched }) {
         <Box border={1} maxWidth='300px' width='100%' height='350px' p={2} m={3}>
             <Form>
                 <Box display='flex' flexDirection='column' justifyContent='space-between'>
-                    <h2 style={{textAlign:'center'}}>Registration</h2>
+                    <h2 style={{textAlign:'center', marginTop:'0'}}>Registration</h2>
+
                     <Field type='text' name='name' placeholder='Name'/>
                     {touched.name && errors.name && <p className='err'>{errors.name}</p>}
+
                     <Field type='email' name='email' placeholder='Email' style={{marginTop:'1rem'}}/>
                     {touched.email && errors.email && <p className='err'>{errors.email}</p>}
+
                     <Field type='password' name='password' placeholder='Password' style={{marginTop:'1rem'}}/>
                     {touched.password && errors.password && <p className='err'>{errors.password}</p>}
+
+                    <Field component='select' name='role' style={{marginTop:'1rem'}}>
+                        <option value='roleSelect' disabled>Please Select a Role</option>
+                        <option value='Admin'>Admin</option>
+                        <option value='Staff'>Staff</option>
+                        <option value='Student'>Student</option>
+                        <option value='Guest'>Guest</option>
+                    </Field>
+                    {touched.role && errors.role && <p className='err'>{errors.role}</p>}
+
                     <label>
                         <Field type='checkbox' name='tos' checked={values.tos} style={{marginTop:'1rem'}}/>
                         Accept Terms of Service
                     </label>
                     {touched.tos && errors.tos && <p className='err'>{errors.tos}</p>}
+
                     <button type='submit' style={{marginTop:'1rem'}}>Submit</button>
                 </Box>
             </Form>
@@ -32,11 +46,12 @@ function RegistrationForm({ values, errors, touched }) {
 }
 
 const FormikRegistrationForm = withFormik({
-    mapPropsToValues({ name, email, password, tos }) {
+    mapPropsToValues({ name, email, password, role, tos }) {
         return {
             name: name || '',
             email: email || '',
             password: password || '',
+            role: role || 'roleSelect',
             tos: tos || false,
         }
     },
@@ -53,6 +68,9 @@ const FormikRegistrationForm = withFormik({
             .string()
             .min(6, '*Password must be 6 characters or longer')
             .required('*Password is required'),
+        role: yup
+            .string()
+            .matches(/(Admin|Staff|Student|Guest)/, '*Role is required'),
         tos: yup
             .boolean()
             .oneOf([true], '*Accept Terms of Service to continue.')
